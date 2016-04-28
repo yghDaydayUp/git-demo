281d71a64986528c351503cf127bc6d0b36e4617
yghDaydayUp <ygh7573418@163.com>
2016-04-28 17:25:17 +0800

    Initial Commit

---
+++ OwenYangProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OwenYangProject
+//
+//  Created by YangGH on 16/4/28.
+//  Copyright © 2016年 YangGH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OwenYangProjectTests/OwenYangProjectTests.m
@@ -0,0 +1,39 @@
+//
+//  OwenYangProjectTests.m
+//  OwenYangProjectTests
+//
+//  Created by YangGH on 16/4/28.
+//  Copyright © 2016年 YangGH. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OwenYangProjectTests : XCTestCase
+
+@end
+
+@implementation OwenYangProjectTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
